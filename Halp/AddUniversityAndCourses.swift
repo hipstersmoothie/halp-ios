9412b7fe7a982216b6ba3b9ecb84cf64d694c99c
Andrew <lisowski54@gmail.com>
2015-08-26 21:31:47 -0700

    focus

--- Halp/AddUniversityAndCourses.swift
+++ Halp/AddUniversityAndCourses.swift
@@ -205,4 +205,25 @@ class AddUniversityAndCourses: UIViewController, MPGTextFieldDelegate, ZFTokenFi
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
         self.view.endEditing(true);
     }
+    
+    
+    var moved:CGFloat!
+    func tokenFieldDidBeginEditing(tokenField:ZFTokenField) {
+        moved = 100 + tokenField.frame.height
+        animateViewMoving(true, moveValue: moved)
+    }
+    
+    func tokenFieldDidEndEditing(tokenField:ZFTokenField) {
+        animateViewMoving(false, moveValue: moved)
+    }
+    
+    func animateViewMoving (up:Bool, moveValue :CGFloat){
+        var movementDuration:NSTimeInterval = 0.3
+        var movement:CGFloat = ( up ? -moveValue : moveValue)
+        UIView.beginAnimations( "animateView", context: nil)
+        UIView.setAnimationBeginsFromCurrentState(true)
+        UIView.setAnimationDuration(movementDuration )
+        self.view.frame = CGRectOffset(self.view.frame, 0,  movement)
+        UIView.commitAnimations()
+    }
 }
